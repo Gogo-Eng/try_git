b38835b7f6b856d5be24bbed3e95dafd5b1137ef
Gogo-Chinda <progressgogochinda@gmail.com>
2023-10-10 15:00:09 +0100

    it will not all happen in one day

---
+++ training_examples_gogo/conditional_statement/for_looping.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+	int main(void) {
+
+	int a;
+	for(a=10; a < 20; a++) {
+		printf("value of a: %d\n", a);
+	}
+	
+	return (0);
+}
+
---
+++ training_examples_gogo/conditional_statement/while_looping.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (void) {
+	
+	int a = 10;
+
+	while (a < 20) {
+		printf("value of a %d\n", a);
+	        a++;	
+		
+	}
+	
+	return 0;
+}
